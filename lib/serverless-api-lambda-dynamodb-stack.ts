cc0407917b50300789e687d9f9463017c3a8096b
Mik Linson <miklinson@gmail.com>
2021-05-30 14:51:32 +0800

    feat: Add Lambda function

--- lib/serverless-api-lambda-dynamodb-stack.ts
+++ lib/serverless-api-lambda-dynamodb-stack.ts
@@ -1,9 +1,24 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as apigateway from '@aws-cdk/aws-apigateway';
 
 export class ServerlessApiLambdaDynamodbStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const table = new dynamodb.Table(this, 'MyTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST
+    });
+
+    const myDynamoLambda = new lambda.Function(this, 'MyDynamoLambda', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lib/lambda'),
+      handler: "function.handler",
+      environment: {
+        HELLO_TABLE_NAME: table.tableName,
+      }
+    });
   }
 }
